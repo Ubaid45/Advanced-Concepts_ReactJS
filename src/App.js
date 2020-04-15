de7434a932d13357980f2f2f14104594f35668ff
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-15 17:17:38 +0200

    Fetching data with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Movie from './hoc/Movie';
-import Counter from './hooks/Counter';
+import Users from './hooks/Users';
 
 function App() {
   return (
-    <Counter id={1}/>
+    <Users/>
   );
 }
 
