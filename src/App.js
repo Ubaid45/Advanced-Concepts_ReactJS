d2b141d22f4995cb8e15b2843c84d44d53dd03ba
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-15 18:01:20 +0200

    updating the context on login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import MoviePage from "./context/MoviePage";
 import UserContext from './context/userContext';
+import Login from './context/Login';
 
 class App extends Component {
-  state = {currentUser: {name: "Ubaid"} };
+  handleLoggedIn = userName => {
+    console.log("Getting the user: "+ userName);
+    const user = {name: "Ubaid Rana"};
+    this.setState({currentUser: user});
+
+  }
+  state = {currentUser: {name: null }};
   render() {
     return (
       <div>
-        <UserContext.Provider value={this.state.currentUser}>
+        <UserContext.Provider value={{
+          currentUser: this.setState.currentUser, 
+          onLoggedIn: this.handleLoggedIn}}>
           <div>
             <MoviePage />
+            <Login />
           </div>
         </UserContext.Provider>
       </div>
