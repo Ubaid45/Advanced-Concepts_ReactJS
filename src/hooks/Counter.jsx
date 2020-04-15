24f5fe758620f744bed78146184758f675dcd826
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-15 17:10:07 +0200

    useEffect hook

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -1,11 +1,11 @@
 import React, { useState, Fragment } from "react";
-//import useDocumentTitle from "./useDocumentTitle";
+import useDocumentTitle from "./useDocumentTitle";
 
 function Counter(props) {
   const [count, setState] = useState(0);
   const [name, setName] = useState("");
 
-  //useDocumentTitle(`${name} has clicked ${count} times!`);
+  useDocumentTitle(`${name} has clicked ${count} times!`);
 
   return (
     <Fragment>
